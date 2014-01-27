a4944f6d5b42eb26effb4496048f1139aa3fd289
System User <pradeesh.login@gmail.com>
2014-01-27 03:59:00 -0600

    Sample Android Program

---
+++ SqliteDemo/src/com/pradeeshmp/sqlitedemo/DbHelper.java
@@ -0,0 +1,37 @@
+package com.pradeeshmp.sqlitedemo;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+/**
+ * sqlite database helper to create table into SQLite database
+ * @author ketan(Visit my <a
+ *         href="http://androidsolution4u.blogspot.in/">blog</a>)
+ */
+public class DbHelper extends SQLiteOpenHelper {
+	static String DATABASE_NAME="userdata";
+	public static final String TABLE_NAME="user";
+	public static final String KEY_FNAME="fname";
+	public static final String KEY_LNAME="lname";
+	public static final String KEY_ID="id";
+	
+	public DbHelper(Context context) {
+		super(context, DATABASE_NAME, null, 1);
+		
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		String CREATE_TABLE="CREATE TABLE "+TABLE_NAME+" ("+KEY_ID+" INTEGER PRIMARY KEY, "+KEY_FNAME+" TEXT, "+KEY_LNAME+" TEXT)";
+		db.execSQL(CREATE_TABLE);
+
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+		onCreate(db);
+
+	}
+
+}
